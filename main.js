2fdd2ee5a1e5f65c0088434a95db187b16107c02
Suhye5n <sy1599@gmail.com>
2022-05-03 01:26:55 +0900

    생성자함수

--- main.js
+++ main.js
@@ -204,3 +204,22 @@ function timeout(cb) {
 timeout(() => {
   console.log('Done!')
 })
+
+
+//생성자 함수
+
+function User(first, last) {
+  this.firstName = first
+  this.lastName = last
+}
+User.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`
+}
+
+const heropy = new User('Heropy', 'Park')
+const amy = new User('Amy', 'Clarke')
+const neo = new User('Neo', 'Smith')
+
+console.log(heropy.getFullName())
+console.log(amy.getFullName())
+console.log(neo.getFullName())
